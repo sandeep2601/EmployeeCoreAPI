d2eb6afe48f6ac62a2ba4690944c63cb3fd88f81
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-07 15:55:41 +0530

    Created the context class and added the connection string

---
+++ EmployeeCoreAPI/Model/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCoreAPI.Model
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<TblEmployee> TblEmployee { get; set; }
+        public DbSet<TblDesignation> TblDesignation { get; set; }
+    }
+}
