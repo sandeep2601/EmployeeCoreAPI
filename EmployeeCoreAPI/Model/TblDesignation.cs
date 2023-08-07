5ad0aa25e6846a73a37fac92898c89b4d080ad6c
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-07 15:41:41 +0530

    Created the Employee and Designation model classes with proper annotations

---
+++ EmployeeCoreAPI/Model/TblDesignation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeCoreAPI.Model
+{
+    public class TblDesignation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string? Designation { get; set; }
+    }
+}
---
+++ EmployeeCoreAPI/Model/TblEmployee.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeCoreAPI.Model
+{
+    public class TblEmployee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [StringLength(250)]
+        public string? Email { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Doj { get; set; }
+
+        [StringLength(20)]
+        public string? Gender { get; set; }
+
+        public int IsMarried { get; set; }
+
+        public int IsActive { get; set; }
+
+        public int DesignationID { get; set; }
+
+        [NotMapped]
+        public string? Designation { get; set; }
+    }
+}
