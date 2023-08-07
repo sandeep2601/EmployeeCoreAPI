6ed972fce3922d2606c150acf27c8dd0fa6c2958
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-07 16:06:22 +0530

    Created the Employee and Designation controllers using scaffolding

---
+++ EmployeeCoreAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployeeCoreAPI.Model;
+
+namespace EmployeeCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblEmployee>>> GetTblEmployee()
+        {
+            if (_context.TblEmployee == null)
+            {
+                return NotFound();
+            }
+            return await _context.TblEmployee.ToListAsync();
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblEmployee>> GetTblEmployee(int id)
+        {
+            if (_context.TblEmployee == null)
+            {
+                return NotFound();
+            }
+            var tblEmployee = await _context.TblEmployee.FindAsync(id);
+
+            if (tblEmployee == null)
+            {
+                return NotFound();
+            }
+
+            return tblEmployee;
+        }
+
+        // PUT: api/Employee/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblEmployee(int id, TblEmployee tblEmployee)
+        {
+            if (id != tblEmployee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblEmployee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblEmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employee
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblEmployee>> PostTblEmployee(TblEmployee tblEmployee)
+        {
+            if (_context.TblEmployee == null)
+            {
+                return Problem("Entity set 'EmployeeContext.TblEmployee'  is null.");
+            }
+            _context.TblEmployee.Add(tblEmployee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTblEmployee", new { id = tblEmployee.Id }, tblEmployee);
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblEmployee(int id)
+        {
+            if (_context.TblEmployee == null)
+            {
+                return NotFound();
+            }
+            var tblEmployee = await _context.TblEmployee.FindAsync(id);
+            if (tblEmployee == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblEmployee.Remove(tblEmployee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TblEmployeeExists(int id)
+        {
+            return (_context.TblEmployee?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
